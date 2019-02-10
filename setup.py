db5f4d115d37dd76693184da620d31d15158d314
grace068 <grace.mywei@gmail.com>
2019-02-09 19:39:39 -0700

    about_me

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='about_me',
+    version='1.0',
+    packages=['about_me'],
+    url='https://github.com/grace068/project1.git',
+    license='',
+    author='Grace Wei',
+    author_email='grace.mywei@gmail.com',
+    description='my python project',
+    install_requires=[
+    ],
+)
